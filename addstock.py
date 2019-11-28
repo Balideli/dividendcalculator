06fb7053a5666cae498304290e0f10db763b7582
Balideli <remynoordzij@hotmail.com>
2019-11-28 15:14:08 +0100

    commit

---
+++ addstock.py
@@ -0,0 +1,28 @@
+
+var stockArray[];
+
+
+function Stock(name, value, dividend) {
+  self.name = name;
+  self.value = value;
+  self.dividend = dividend;
+
+  return this;
+}
+
+
+var stock1 = new Stock('ADM', 1000, 2);
+
+stock1 = stockArray[0];
+
+
+
+
+
+
+function calculateValue{
+  for(i=0; i<stockArray.length; i++;) {
+    portfolioValue += stockArray[i].value;
+  }
+  return portfolioValue;
+}
---
+++ dividend.py
@@ -0,0 +1,48 @@
+#python
+
+
+#store user input dividend information in array
+
+dividend_array = []
+
+count = 0
+
+user_input = input("Enter the dividend: ")
+count +=
+
+dividend_array.append(user_input)
+
+
+
+#Now add together and divide for average
+
+
+total = 0
+
+for dividend in range len(dividend_array) :
+    total += dividend
+
+
+average = total/len(dividend_array)
+
+
+
+    print(f"Total average dividend is: {average}.")
+
+
+
+
+
+
+
+
+
+
+
+
+function calculateDividend{
+  for(i=0; i<stockArray.length; i++){
+    totalDividend += stockArray[i].dividend;
+  }
+  return totalDividend;
+}
